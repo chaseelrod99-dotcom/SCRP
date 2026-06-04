e204c263388666f3b21e2886eaa641d893fd1ff5
chaseelrod99-dotcom <chaseelrod99@gmail.com>
2026-06-04 10:36:02 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "",
+        name: "Made with ❤️",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
